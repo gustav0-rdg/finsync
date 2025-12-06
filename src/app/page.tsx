508a1489e8366895f486d919d80423df4ef92d36
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-12-06 16:41:52 -0300

    ADD: Criando outras páginas pra exibir quando clicar no botão da Aside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ import { useState } from "react";
 import Aside from "@/components/aside/Aside"
 import HomePage from "@/pages/home/HomePage";
 import History from "@/pages/history/History"
-
-  
+import Me from "@/pages/me/Me";
+import Dashboard from "@/pages/dashboard/dashboard";
+import Finances from "@/pages/finances/Finances";
 
 export default function Home(){
   const [page, setPage] = useState(1)
@@ -16,9 +17,14 @@ export default function Home(){
       case 1:
       default:
         return <HomePage />
+      case 2:
+        return <Finances />
+      case 3:
+        return <Dashboard />
       case 4:
         return <History />
-      
+      case 5:
+        return <Me />
     }
   }
 
